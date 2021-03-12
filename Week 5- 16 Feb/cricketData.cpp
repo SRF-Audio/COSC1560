1be94c23b0b573ca34ca8583255955076ca194fe
Stephen Froeber <srfaudioproductions@gmail.com>
2021-03-12 08:56:40 -0600

    Start of assignment 6

--- Week 5- 16 Feb/cricketData.cpp
+++ Week 5- 16 Feb/cricketData.cpp
@@ -32,6 +32,7 @@ int main()
     int numStudents; //Dynamic value from user
     string *sNamesPtr = nullptr;
     int *iMatchesPtr = nullptr;
+    double average;
 
     //program title
     cout << "Cricket Matches in a Year" << endl;
@@ -54,12 +55,20 @@ int main()
 
     display(sNamesPtr, iMatchesPtr, numStudents); //display what the user just entered
 
-    mostMatches(sNamesPtr, iMatchesPtr, numStudents);
+    mostMatches(sNamesPtr, iMatchesPtr, numStudents); //calculate and display most matches
+
+    average = mean(iMatchesPtr, numStudents);
+    cout << "\nThe mean number of matches played by all students is " << average << " matches." << endl;
+
+    sortStudents(sNamesPtr, iMatchesPtr, numStudents); //sort the names and matches
+
+    display(sNamesPtr, iMatchesPtr, numStudents); //display the sorted array
 
     return 0;
 }
 
 //Function definitions
+// Allow the user to enter the nameof each student and number of cricket matches played by thatstudentand store this information in the twoarrays.
 void input(string names[], int matches[], int size)
 {
     //function intro
@@ -86,15 +95,18 @@ void input(string names[], int matches[], int size)
     }
 }
 
+//display names of students and number of matches each has played
 void display(const string names[], const int matches[], int size)
 {
-    cout << "You entered:" << endl;
+    cout << "\nCurrent List:" << endl;
+    //display loop
     for (int i = 0; i < size; i++)
     {
         cout << names[i] << " played " << matches[i] << " matches." << endl;
     }
 }
 
+//Display the name of the student who played the most cricket matches in the year, and the number of matches they played.
 void mostMatches(const string names[], const int matches[], int size)
 {
     int most = 0,
@@ -109,19 +121,41 @@ void mostMatches(const string names[], const int matches[], int size)
     }
     cout << "Of the students you entered, " << names[index] << " played the most matches, with " << most << " total matches.";
 }
+
+//The function should determine the meanof the array, and return this valueas type double.
 double mean(const int matches[], int num)
 {
-    static int sum;
-    double mean = static_cast<double>(sum) / num;
+    static int sum = 0;
 
     for (int i = 0; i < num; i++)
     {
         sum += matches[i];
     }
 
+    double mean = static_cast<double>(sum) / num;
     return mean;
 }
 
 void sortStudents(string names[], int matches[], int size)
 {
+    int minIndex, minValue; //will store array index, and the value of matches
+    string minName;         //stores the names
+
+    for (int start = 0; start < size - 1; start++)
+    {
+        minIndex = start;
+        minValue = matches[start];
+        minName = names[start];
+        for (int index = start + 1; index < size; index++)
+        {
+            if (names[index] < minName)
+            {
+                minName = names[index];
+                minValue = matches[index];
+                minIndex = index;
+            }
+        }
+        swap(names[minIndex], names[start]);
+        swap(matches[minIndex], matches[start]);
+    }
 }
