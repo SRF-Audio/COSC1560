bd86e955f537d9693b73b3def4eac7f44ad7f3e4
Stephen Froeber <srfaudioproductions@gmail.com>
2021-04-04 10:58:45 -0500

    Added name display function

--- Week 7- 19 Mar/speakersBureau.cpp
+++ Week 7- 19 Mar/speakersBureau.cpp
@@ -20,6 +20,7 @@
 
 using namespace std;
 
+//Structures
 struct Speaker
 {
     string name;
@@ -28,20 +29,33 @@ struct Speaker
     double fee;
 };
 
-void dataEntry(Speaker speakerList[], const int SPEAKERS, int &userSpeakers);           //populate the full array of Speakers
-void changeSpeaker(Speaker speakerList[], const int userSpeakers, string userEditName); //Edit details of one speaker
+//Global Variables
+const int numDigits = 10; //phone number length for US numbers
+const int SPEAKERS = 10;  //max Speakers
 
-void nameValidator(Speaker speakerList[]); //make sure names are not empty
+//Function prototypes
+void dataEntry(Speaker speakerList[], const int SPEAKERS, int &userSpeakers); //populate the full array of Speakers
+void nameDisplay(Speaker speakerList[], const int &userSpeakers);
+void changeSpeaker(Speaker speakerList[], const int userSpeakers, string userEditName); //Edit details of one speaker
+void displaySpeaker(Speaker speakerList[], int position, string displaySpeakerInfo);    //display a single speaker's info
+
+//validator functions
+void nameValidator(Speaker speakerList[], int position);          //Validate names are not empty
+void phoneNumberValidator(Speaker speakerList[], int position);   //Validate number of digits
+void phoneNumberFormatterUS(Speaker speakerList[], int position); //convert string to US phone number format
+void topicValidator(Speaker speakerList[], int position);         //validate topic isn't empty
+void feeValidator(Speaker speakerList[], int position);           //validate fee isn't negative
 
 int main()
 {
     cout << "Speaker's Bureau Database" << endl;
 
     //create the storage mechanism for users to edit speakers
-    const int SPEAKERS = 10;       //max Speakers
+
     Speaker speakerList[SPEAKERS]; //storage array
     int userSpeakers;              //custom number of speakers
     string userEditName;           //for editing a particular speaker
+    string displaySpeakerInfo;     //for displaying a particular speaker
 
     //get user input and validate that it's not zero
     cout << "\nHow many speakers do you have? ";
@@ -59,21 +73,24 @@ int main()
 
     dataEntry(speakerList, SPEAKERS, userSpeakers);
 
-    cout << "\nThe database currently has the following speakers:" << endl;
-    for (int i = 0; i < userSpeakers; i++)
-    {
-        cout << speakerList[i].name << endl;
-    }
-    cout << "Which Speaker would you like to edit? " << endl;
+    nameDisplay(speakerList, userSpeakers);
+
+    cout << "Which Speaker would you like to edit? ";
     cin.clear();
     getline(cin, userEditName);
+
+    changeSpeaker(speakerList, userSpeakers, userEditName);
 
     return 0;
 }
 
+/* 
+Each function should accept an array as the first argument.  In the first function, the second argument is the current size of the array.  In the other functions, the second argument is the number of speakers currently stored in the array.  Additional arguments may be needed for some functions.
+*/
+
 void dataEntry(Speaker speakerList[], const int SPEAKERS, int &userSpeakers)
 {
-    int numDigits = 10;
+
     //populate the structure array with the user selected number of speakers
     for (int i = 0; i < userSpeakers; i++)
     {
@@ -85,59 +102,43 @@ void dataEntry(Speaker speakerList[], const int SPEAKERS, int &userSpeakers)
         cin.clear();
         getline(cin, speakerList[i].name);
 
-        nameValidator(speakerList[], i);
+        nameValidator(speakerList, i);
 
         //Phone Number block
         cout << "\nEnter " << speakerList[i].name << "'s phone number (enter 10 digits, with no dashes or hyphens): ";
         cin.clear();
         getline(cin, speakerList[i].phoneNumber);
 
-        //Phone number validation block
-        while (speakerList[i].phoneNumber.length() != numDigits)
-        {
-            cout << "\nPlease enter exactly 10 digits, with no spaces, hyphens, or parentheses: ";
-            cin.clear();
-            getline(cin, speakerList[i].phoneNumber);
-        }
+        phoneNumberValidator(speakerList, i);
 
-        //US phone number formatter
-        speakerList[i].phoneNumber.insert(0, "(");
-        speakerList[i].phoneNumber.insert(4, ")");
-        speakerList[i].phoneNumber.insert(9, "-");
-        // Outputs: (314)308-1307
+        phoneNumberFormatterUS(speakerList, i);
 
         //Topic block
         cout << "\nEnter " << speakerList[i].name << "'s speaking topic(s): ";
         cin.clear();
         getline(cin, speakerList[i].topic);
 
-        //Topic validation
-        while (speakerList[i].topic.length() == 0)
-        {
-            cout << "\nYou must enter a topic: ";
-            cin.clear();
-            getline(cin, speakerList[i].topic);
-        }
+        topicValidator(speakerList, i);
 
         //Fee block
         cout << "\nEnter " << speakerList[i].name << "'s speaking fee: $";
         cin.clear();
         cin >> speakerList[i].fee;
 
-        //fee validation
-        while (speakerList[i].fee < 0)
-        {
-            cout << "\nThe fee must be a positive dollar amount: $";
-            cin >> speakerList[i].fee;
-        }
+        feeValidator(speakerList, i);
     }
 }
 
-/* 
-To do list:
+void nameDisplay(Speaker speakerList[], const int &userSpeakers)
+{
+    cout << "\nThe database currently has the following speakers:" << endl;
+    for (int i = 0; i < userSpeakers; i++)
+    {
+        cout << speakerList[i].name << endl;
+    }
+}
 
-Each function shouldaccept an array as the firstargument.  In the first function, the second argument is the current size of the array.  In the other functions, the second argument is the number of speakers currently stored in the array.  Additional arguments may be needed for some functions.
-
+/*
 ii)Change the contents fora particular speaker who is already stored in the array.The name of the speaker being updated will be passed as the third argument.
 */
 
@@ -155,20 +156,38 @@ void changeSpeaker(Speaker speakerList[], const int userSpeakers, string userEdi
     cout << "\nEnter the new name for Speaker " << editPosition + 1 << ": ";
     cin.clear();
     getline(cin, speakerList[editPosition].name);
-    cout << "\nEnter the new phone number for Speaker " << editPosition + 1 << ": ";
+    nameValidator(speakerList, editPosition);
+
+    cout << "\nEnter the new phone number for " << speakerList[editPosition].name << ": ";
     cin.clear();
     getline(cin, speakerList[editPosition].phoneNumber);
-    cout << "\nEnter the new topic for Speaker " << editPosition + 1 << ": ";
+    phoneNumberValidator(speakerList, editPosition);
+
+    cout << "\nEnter the new topic for " << speakerList[editPosition].name << ": ";
     cin.clear();
     getline(cin, speakerList[editPosition].topic);
-    cout << "\nEnter the new fee for Speaker " << editPosition + 1 << ": $";
+    topicValidator(speakerList, editPosition);
+
+    cout << "\nEnter the new fee for " << speakerList[editPosition].name << ": $";
     cin.clear();
     cin >> speakerList[editPosition].fee;
+    feeValidator(speakerList, editPosition);
 }
 
 /*
 iii)Display a specific speaker’s information.The name of the speaker to be displayed will be passed as the third argument.
+*/
 
+void displaySpeaker(Speaker speakerList[], int userSpeakers, string displaySpeakerInfo)
+{
+    cout << "\nThe database currently has the following speakers:" << endl;
+    for (int i = 0; i < userSpeakers; i++)
+    {
+        cout << speakerList[i].name << endl;
+    }
+}
+
+/*
 iv)Displaythe people speaking about a specific topic.The name of the specific topic should be passed as the third argument.  All speakers whoare speaking about this topicshould be displayed.  If no speakers are speaking about this topic thenan appropriate message should be displayed.
 
 v)Display all the data stored in the array.
@@ -177,11 +196,48 @@ v)Display all the data stored in the array.
 //validators
 void nameValidator(Speaker speakerList[], int position)
 {
-    //name validation
     while (speakerList[position].name.length() == 0)
     {
         cout << "\nYou must enter a name: ";
         cin.clear();
         getline(cin, speakerList[position].name);
+    }
+}
+
+void phoneNumberValidator(Speaker speakerList[], int position)
+{
+    while (speakerList[position].phoneNumber.length() != numDigits)
+    {
+        cout << "\nPlease enter exactly 10 digits, with no spaces, hyphens, or parentheses: ";
+        cin.clear();
+        getline(cin, speakerList[position].phoneNumber);
+    }
+}
+
+void phoneNumberFormatterUS(Speaker speakerList[], int position)
+{
+    speakerList[position].phoneNumber.insert(0, "(");
+    speakerList[position].phoneNumber.insert(4, ")");
+    speakerList[position].phoneNumber.insert(9, "-");
+    // Outputs: (314)308-1307
+}
+
+void topicValidator(Speaker speakerList[], int position)
+{
+    while (speakerList[position].topic.length() == 0)
+    {
+        cout << "\nYou must enter a topic: ";
+        cin.clear();
+        getline(cin, speakerList[position].topic);
+    }
+}
+
+void feeValidator(Speaker speakerList[], int position)
+{
+    //fee validation
+    while (speakerList[position].fee < 0)
+    {
+        cout << "\nThe fee must be a positive dollar amount: $";
+        cin >> speakerList[position].fee;
     }
 }
